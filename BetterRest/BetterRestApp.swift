64ef36a5f27a6c5a0450e3a2019dade024df20c6
Anthony Candelino <anthcandelino@gmail.com>
2024-06-22 09:12:31 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Anthony Candelino on 2024-06-22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
